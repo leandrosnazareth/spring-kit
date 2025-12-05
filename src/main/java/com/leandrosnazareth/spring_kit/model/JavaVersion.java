6c511fbe228c275714aebd1bb2d154183781acc3
Leandro Nazareth <leandrosnazareth@gmail.com>
2025-12-05 20:54:07 -0300

    version

--- src/main/java/com/leandrosnazareth/spring_kit/model/JavaVersion.java
+++ src/main/java/com/leandrosnazareth/spring_kit/model/JavaVersion.java
@@ -6,11 +6,50 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum JavaVersion {
-    JAVA_8("8", "Java 8 (LTS)", new String[]{"2.0.9.RELEASE", "2.1.18.RELEASE", "2.2.13.RELEASE", "2.3.12.RELEASE"}),
-    JAVA_11("11", "Java 11 (LTS)", new String[]{"2.3.12.RELEASE", "2.4.13", "2.5.15", "2.6.15", "2.7.18"}),
-    JAVA_17("17", "Java 17 (LTS)", new String[]{"2.7.18", "3.0.13", "3.1.12", "3.2.5", "3.3.0", "3.5.9-SNAPSHOT", "4.0.0", "4.0.1-SNAPSHOT"}),
-    JAVA_21("21", "Java 21 (LTS)", new String[]{"3.2.5", "3.3.0", "3.5.9-SNAPSHOT", "4.0.0", "4.0.1-SNAPSHOT"}),
-    JAVA_25("25", "Java 25 (LTS)", new String[]{"3.5.9-SNAPSHOT", "4.0.0", "4.0.1-SNAPSHOT"});
+JAVA_8("8", "Java 8 (LTS)", 
+        new String[]{
+                "2.3.12.RELEASE",
+                "2.2.13.RELEASE",
+                "2.1.18.RELEASE",
+                "2.0.9.RELEASE"
+        }),
+
+JAVA_11("11", "Java 11 (LTS)", 
+        new String[]{
+                "2.7.18",
+                "2.6.15",
+                "2.5.15",
+                "2.4.13",
+                "2.3.12.RELEASE"
+        }),
+
+JAVA_17("17", "Java 17 (LTS)", 
+        new String[]{
+                "4.0.1-SNAPSHOT",
+                "4.0.0",
+                "3.5.9-SNAPSHOT",
+                "3.3.0",
+                "3.2.5",
+                "3.1.12",
+                "3.0.13",
+                "2.7.18"
+        }),
+
+JAVA_21("21", "Java 21 (LTS)", 
+        new String[]{
+                "4.0.1-SNAPSHOT",
+                "4.0.0",
+                "3.5.9-SNAPSHOT",
+                "3.3.0",
+                "3.2.5"
+        }),
+
+JAVA_25("25", "Java 25 (LTS)", 
+        new String[]{
+                "4.0.1-SNAPSHOT",
+                "4.0.0",
+                "3.5.9-SNAPSHOT"
+        });
 
     private final String version;
     private final String label;
